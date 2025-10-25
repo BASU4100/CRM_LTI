d5f3624bec507a118c095a54cc374807645feb84
username <wecp-dummy@gmail.com>
2025-10-25 12:29:52 +0000

    brat's login n get-booking empty methods

--- client/src/app/get-bookings/get-bookings.component.ts
+++ client/src/app/get-bookings/get-bookings.component.ts
@@ -1,4 +1,5 @@
 import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,6 +12,39 @@ import { HttpService } from '../../services/http.service';
   styleUrls: ['./get-bookings.component.scss']
 })
 export class GetBookingsComponent {
-  itemForm:FormGroup
-  
+  itemForm! : FormGroup
+  formModel : any
+  showError : boolean=false
+  errorMessage : boolean=false
+  carList : any[] = [] 
+  assignModel : any
+  showMessage : any
+  responseMessage : any
+  updateId : any
+  toBook : any
+  idPaymentNow : any
+  selectedBooking : any
+
+  constructor(private fb:FormBuilder,private httpService:HttpService, private router:Router,private authService:AuthService,private datePipe:DatePipe){}
+
+  ngOnInit():void{
+    this.itemForm = this.fb.group({
+      rentalStartDate:['',Validators.required],
+      rentalEndDate:['',Validators.required]
+    })
+  }
+  onSubmit(){
+    
+  }
+
+  getBookings(){
+
+  }
+  bookNow(val){
+
+  }
+  payment(val){
+
+  }
+
 }

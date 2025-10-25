148250367d04f800b021948785a54ee0280706c7
username <wecp-dummy@gmail.com>
2025-10-25 10:42:41 +0000

    Adi's Day 1

--- server/src/main/java/com/wecp/car_rental_management_system/entity/User.java
+++ server/src/main/java/com/wecp/car_rental_management_system/entity/User.java
@@ -5,7 +5,84 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 import java.util.List;
 
+
 @Table(name = "users") // do not change this line i.e table name
+@Entity
 public class User {
-    // implement entity
+    // Attributes of User entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true)
+    String username;
+    String password;
+    @Column(unique = true)
+    String email;
+    String role;
+    @OneToMany(mappedBy = "users")
+    List<Booking> bookings;
+
+    //Constructors
+    public User() {
+    }
+
+    
+    public User(String username, String password, String email, String role, List<Booking> bookings) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.bookings = bookings;
+    }
+
+
+    public User(Long id, String username, String password, String email, String role, List<Booking> bookings) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.bookings = bookings;
+    }
+
+    //Getters and Setters method
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    
 }

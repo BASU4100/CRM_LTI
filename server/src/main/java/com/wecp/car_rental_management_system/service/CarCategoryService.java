de0e2c6bb3dd130193421cd91f78b5d8f34558e0
username <wecp-dummy@gmail.com>
2025-10-25 07:31:28 +0000

    CarCategory,Administrator Controller Updated

--- server/src/main/java/com/wecp/car_rental_management_system/service/CarCategoryService.java
+++ server/src/main/java/com/wecp/car_rental_management_system/service/CarCategoryService.java
@@ -8,8 +8,28 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-
+@Service
 public class CarCategoryService {
     // implement car category service
+    @Autowired
+    CarCategoryRepository carCategoryRepository;
+
+    //create Car Category
+    public CarCategory createCarCategory(CarCategory carCategory){
+        return  carCategoryRepository.save(carCategory);
+    }
+    //Get all car categories
+    public List<CarCategory> getAllCarCategories(){
+        return carCategoryRepository.findAll();
+    }
+
+    //update car category
+    public CarCategory updateCarCategory(Long categoryId,CarCategory updatedCarCategory){
+        CarCategory carCategory=carCategoryRepository.findById(categoryId).get();
+        if(carCategory !=null && carCategory.getId()!=updatedCarCategory.getId()){
+            return null;
+        }
+            return carCategoryRepository.save(updatedCarCategory);
+    }
 }
 

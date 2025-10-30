9ba72a7cea9e8c0e65ab2b946a7d1bb766900d5c
username <wecp-dummy@gmail.com>
2025-10-30 17:19:48 +0000

    working on dashboard

--- client/src/app/dashbaord/dashbaord.component.ts
+++ client/src/app/dashbaord/dashbaord.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from '../../environments/environment.development';
 import { AuthService } from '../../services/auth.service';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-dashbaord',
@@ -10,12 +9,16 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./dashbaord.component.scss']
 })
 export class DashbaordComponent implements OnInit {
-  constructor(private router: Router, private authService: AuthService) {}
+  userDetails: any;
+
+  constructor(private router: Router, private authService: AuthService, private httpService: HttpService) {}
   
   // check protect component when logged out
   ngOnInit(): void {
     if(!this.authService.getLoginStatus) {
       this.router.navigate(['/login']);
     }
+    const userId = this.authService.getUserId();
+    this.userDetails = this.httpService.getUserDetails(userId);
   }
 }
